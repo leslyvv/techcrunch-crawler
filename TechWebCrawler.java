67da3f21b16d5b9c3572633dc2712ea0ab63086f
Lesly Vargas <117410261+leslyvv@users.noreply.github.com>
2023-11-14 12:52:05 -0500

    Update TechWebCrawler.java
    Writes into a CSV file

--- TechWebCrawler.java
+++ TechWebCrawler.java
@@ -1,5 +1,6 @@
 package WebCrawler;
-
+import java.util.*;
+import com.opencsv.CSVWriter;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -21,7 +22,8 @@ public class TechWebCrawler implements Runnable{
     private ArrayList<String> visitedLink = new ArrayList<String>();
     private ArrayList<TechArticles> articlesList = new ArrayList<>();
     private int _threadID;
-
+    // prevents concurrency issues
+    public static List<TechArticles>  allThreadsArticlesList = Collections.synchronizedList(new ArrayList<>());
     public TechWebCrawler(String link, int threadID){
         _link = link;
         _threadID = threadID;
@@ -79,7 +81,7 @@ public class TechWebCrawler implements Runnable{
                     //creates a new object every time for each link
                     TechArticles techArticle = new TechArticles(_threadID, title, urls, formattedDate, author, content);
                     articlesList.add(techArticle);
-
+                    allThreadsArticlesList.add(techArticle);
                 }
                 //displays the articles information
                 for (TechArticles art : articlesList) {
@@ -101,6 +103,32 @@ public class TechWebCrawler implements Runnable{
         }
 
         return null;
+    }
+        // writes information to an CSV
+    public static void writeDataToCSV(List<TechArticles> articlesList){
+        String csvFileName = "TechCrunchArticles.csv";
+
+        try(CSVWriter csvWrite = new CSVWriter(new FileWriter(csvFileName))) {
+
+            String[] headers = {"Thread ID", "Title of Article", "URL", "Date and Time", "Author Name", "Content Description"};
+            csvWrite.writeNext(headers);
+
+            for (TechArticles article : articlesList) {
+                String[] data = {
+                        String.valueOf(article.get_thread()),
+                        article.get_title(),
+                        article.get_url(),
+                        article.get_date(),
+                        article.get_author(),
+                        article.get_content()
+                };
+                // starts new line for each url
+                csvWrite.writeNext(data);
+            }
+            System.out.println("Data is successfully written to " + csvFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     private String formatDate(String date) throws ParseException{
         // original date format versus new date
